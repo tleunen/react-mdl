748f2f0719929bb25c3d69c44d915eda5bfc598f
Tommy Leunen <tommy.leunen@appdirect.com>
2015-07-20 19:31:40 -0400

    added multi-lines textfield

--- src/Textfield.js
+++ src/Textfield.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
+import ObjectAssign from 'object-assign';
 
 class Textfield extends React.Component {
     constructor(props) {
@@ -12,6 +13,7 @@ class Textfield extends React.Component {
         this._handleChange = this._handleChange.bind(this);
         this._handleFocus = this._handleFocus.bind(this);
         this._handleBlur = this._handleBlur.bind(this);
+        this._handleKeyDown = this._handleKeyDown.bind(this);
     }
 
     _handleChange(e) {
@@ -33,9 +35,23 @@ class Textfield extends React.Component {
         });
     }
 
+    _handleKeyDown(e) {
+        if(!!this.props.maxRows) {
+            var currentRowCount = event.target.value.split('\n').length;
+            if(event.keyCode === 13) {
+                if(currentRowCount >= this.props.maxRows) {
+                    event.preventDefault();
+                }
+            }
+        }
+    }
+
     render() {
         var id = 'textfield-' + this.props.label.replace(/\s+/g, '');
         var type = this.props.type || 'text';
+        var hasRows = !!this.props.rows;
+        var rows = this.props.rows || 1;
+        if(rows < 1) rows = 1;
 
         var elemClasses = classNames('mdl-textfield', 'is-upgraded', {
             'mdl-textfield--floating-label': !!this.props.floatingLabel,
@@ -45,24 +61,34 @@ class Textfield extends React.Component {
             'is-focused': this.state.isFocused
         });
 
-        var pattern = this.props.pattern || '*';
+        var genericProps = {
+            ref: 'input',
+            className: 'mdl-textfield__input',
+            id: id,
+            value: this.props.value,
+            disabled: this.props.disabled,
+            onChange: this._handleChange,
+            onFocus: this._handleFocus,
+            onBlur: this._handleBlur
+        };
+
+        var field = hasRows || this.props.maxRows > 1 ? (
+            React.createElement('textarea', ObjectAssign(genericProps, {
+                rows: rows,
+                onKeyDown: this._handleKeyDown
+            }))
+        ) : (
+            React.createElement('input', ObjectAssign(genericProps, {
+                type: type
+            }, this.props.pattern ? {
+                pattern: this.props.pattern
+            } : null))
+        );
 
         return (
             <div className={elemClasses}>
-                <input
-                    ref="input"
-                    className="mdl-textfield__input"
-                    type={type}
-                    id={id}
-                    pattern={pattern}
-                    value={this.props.value}
-                    disabled={this.props.disabled}
-                    onChange={this._handleChange}
-                    onFocus={this._handleFocus}
-                    onBlur={this._handleBlur}
-                />
+                {field}
                 <label className="mdl-textfield__label" htmlFor={id}>{this.props.label}</label>
-
                 {this.props.error ? (
                     <span className="mdl-textfield__error">{this.props.error}</span>
                 ) : null}
@@ -76,8 +102,10 @@ Textfield.propTypes = {
     error: PropTypes.string,
     floatingLabel: PropTypes.bool,
     label: PropTypes.string.isRequired,
+    maxRows: PropTypes.number,
     onChange: PropTypes.func.isRequired,
     pattern: PropTypes.string,
+    rows: PropTypes.number,
     type: PropTypes.string,
     value: PropTypes.string
 };
