4765a9ce2be12d0d23a01d1e8b02da6155ca1589
Tommy Leunen <tommy.leunen@appdirect.com>
2015-08-07 20:12:36 -0400

    removes object-assign

--- src/Textfield.js
+++ src/Textfield.js
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
-import objectAssign from 'object-assign';
 
 class Textfield extends React.Component {
     constructor(props) {
