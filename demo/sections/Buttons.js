d9cac8b86ea63434322002e97c8243e4276122d6
Josh Quintana <josh_d_quintana@homedepot.com>
2015-10-18 00:49:54 -0400

    Refactor demo : Buttons

--- demo/sections/Buttons.js
+++ demo/sections/Buttons.js
@@ -6,7 +6,7 @@ import Button from '../../src/Button';
 import IconButton from '../../src/IconButton';
 import FABButton from '../../src/FABButton';
 
-let Colored = ( props ) => (
+let Colored = () => (
     <Example>
         <FABButton colored>
             <Icon name="add" />
@@ -16,59 +16,80 @@ let Colored = ( props ) => (
         </FABButton>
     </Example>
 );
+let FAB = () => (
+    <Example>
+        <FABButton>
+            <Icon name="add" />
+        </FABButton>
+        <FABButton ripple={ false }>
+            <Icon name="add" />
+        </FABButton>
+        <FABButton disabled>
+            <Icon name="add" />
+        </FABButton>
+    </Example>
+);
+
+let Raised = () => (
+    <Example>
+        <Button raised>Button</Button>
+        <Button raised ripple={ false }>Button</Button>
+        <Button raised disabled>Button</Button>
+    </Example>
+);
+
+let RaisedColors = () => (
+    <Example>
+        <Button raised colored>Button</Button>
+        <Button raised accent>Button</Button>
+        <Button raised accent ripple={ false }>Button</Button>
+    </Example>
+);
+
+let Flat = () => (
+    <Example>
+        <Button>Button</Button>
+        <Button ripple={ false }>Button</Button>
+        <Button disabled>Button</Button>
+    </Example>
+);
+
+let FlatColors = () => (
+    <Example>
+        <Button primary colored>Button</Button>
+        <Button accent>Button</Button>
+    </Example>
+);
+
+let Icons = () => (
+    <Example>
+        <IconButton name="mood" />
+        <IconButton colored name="mood" />
+    </Example>
+);
+
+let FABMini = () => (
+    <Example>
+        <FABButton mini>
+            <Icon name="add" />
+        </FABButton>
+        <FABButton colored mini>
+            <Icon name="add" />
+        </FABButton>
+    </Example>
+);
 
 export default ( props ) => (
     <section { ...props }>
         <h3>Buttons</h3>
+
         <Colored />
-
-        <Example>
-            <FABButton>
-                <Icon name="add" />
-            </FABButton>
-            <FABButton ripple={ false }>
-                <Icon name="add" />
-            </FABButton>
-            <FABButton disabled>
-                <Icon name="add" />
-            </FABButton>
-        </Example>
-
-        <Example>
-            <Button raised>Button</Button>
-            <Button raised ripple={ false }>Button</Button>
-            <Button raised disabled>Button</Button>
-        </Example>
-
-        <Example>
-            <Button raised colored>Button</Button>
-            <Button raised accent>Button</Button>
-            <Button raised accent ripple={ false }>Button</Button>
-        </Example>
-
-        <Example>
-            <Button>Button</Button>
-            <Button ripple={ false }>Button</Button>
-            <Button disabled>Button</Button>
-        </Example>
-
-        <Example>
-            <Button primary colored>Button</Button>
-            <Button accent>Button</Button>
-        </Example>
-
-        <Example>
-            <IconButton name="mood" />
-            <IconButton colored name="mood" />
-        </Example>
-
-        <Example>
-            <FABButton mini>
-                <Icon name="add" />
-            </FABButton>
-            <FABButton colored mini>
-                <Icon name="add" />
-            </FABButton>
-        </Example>
+        <FAB />
+        <Raised />
+        <RaisedColors />
+        <Flat />
+        <FlatColors />
+        <Icons />
+        <FABMini />
     </section>
 );
