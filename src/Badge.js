b222ce70d73d2852e60fefe639eb2445b1d67041
Tommy Leunen <tommy.leunen+github@gmail.com>
2015-10-20 20:16:40 -0400

    converts badge to class again because stateless fn cannot return null

--- src/Badge.js
+++ src/Badge.js
@@ -1,37 +1,39 @@
 import React, { PropTypes } from 'react';
 
-var Badge = (props) => {
-    var { children, text } = props;
+class Badge extends React.Component {
+    static propTypes = {
+        children: PropTypes.oneOfType([
+            PropTypes.element,
+            PropTypes.string
+        ]),
+        text: PropTypes.oneOfType([
+            React.PropTypes.string,
+            React.PropTypes.number
+        ])
+    };
 
-    // No badge if no children
-    if(!React.Children.count(children)) return null;
+    render() {
+        var { children, text } = this.props;
 
-    var element;
-    if(typeof children === 'string') {
-        element = <span>{children}</span>;
+        // No badge if no children
+        if(!React.Children.count(children)) return null;
+
+        var element;
+        if(typeof children === 'string') {
+            element = <span>{children}</span>;
+        }
+        else {
+            element = React.Children.only(children);
+        }
+
+        // No text -> No need of badge
+        if(text === null || typeof text === 'undefined') return element;
+
+        return React.cloneElement(element, {
+            className: 'mdl-badge',
+            'data-badge': text
+        });
     }
-    else {
-        element = React.Children.only(children);
-    }
-
-    // No text -> No need of badge
-    if(text === null || typeof text === 'undefined') return element;
-
-    return React.cloneElement(element, {
-        className: 'mdl-badge',
-        'data-badge': text
-    });
-};
-
-Badge.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.element,
-        PropTypes.string
-    ]),
-    text: PropTypes.oneOfType([
-        React.PropTypes.string,
-        React.PropTypes.number
-    ])
-};
+}
 
 export default Badge;
