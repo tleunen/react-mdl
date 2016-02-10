f550b2da0f34952a601f652b90c93196da1627ba
Julien Fradin <julien@frad.in>
2016-02-10 17:20:10 +0100

    Cleaned code

--- src/__tests__/List-test.js
+++ src/__tests__/List-test.js
@@ -10,37 +10,6 @@ describe('List', () => {
 
         expect(output.type).toBe('ul');
     });
-
-    // it('should render an ul list', () => {
-    //     const output = render(<Menu target="none" />);
-
-    //     expect(output.type).toBe('ul');
-    // });
-
-    // it('should be left bottom aligned by default', () => {
-    //     const output = render(<Menu target="none" />);
-
-    //     expect(output.props.className)
-    //         .toInclude('mdl-menu--bottom-left');
-    // });
-
-    // it('should have alignment specific css', () => {
-    //     ['left', 'right'].forEach(align => {
-    //         ['bottom', 'top'].forEach(valign => {
-    //             const output = render(<Menu target="none" align={align} valign={valign} />);
-
-    //             expect(output.props.className)
-    //                 .toInclude(`mdl-menu--${valign}-${align}`);
-    //         });
-    //     });
-    // });
-
-    // it('should have ripple if specified specific css', () => {
-    //     const output = render(<Menu target="none" ripple />);
-
-    //     expect(output.props.className)
-    //         .toInclude('mdl-js-ripple-effect');
-    // });
 });
 
 describe('ListItem', () => {
