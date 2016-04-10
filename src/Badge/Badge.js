c1d85efe87721ed5bb1af30d18861ff35284eed2
Tommy Leunen <tommy.leunen+github@gmail.com>
2016-04-10 16:58:54 -0400

    changed Badge as a class component to keep compatibility with React 0.14

--- src/Badge/Badge.js
+++ src/Badge/Badge.js
@@ -15,27 +15,29 @@ const propTypes = {
     noBackground: PropTypes.bool
 };
 
-const Badge = (props) => {
-    const { children, className, text, overlap, noBackground } = props;
+class Badge extends React.Component {
+    render() {
+        const { children, className, text, overlap, noBackground } = this.props;
 
-    // No badge if no children
-    if (!React.Children.count(children)) return null;
+        // No badge if no children
+        if (!React.Children.count(children)) return null;
 
-    const element = typeof children === 'string'
-        ? <span>{children}</span>
-        : React.Children.only(children);
+        const element = typeof children === 'string'
+            ? <span>{children}</span>
+            : React.Children.only(children);
 
-    // No text -> No need of badge
-    if (text === null || typeof text === 'undefined') return element;
+        // No text -> No need of badge
+        if (text === null || typeof text === 'undefined') return element;
 
-    return React.cloneElement(element, {
-        className: classNames(className, element.props.className, 'mdl-badge', {
-            'mdl-badge--overlap': !!overlap,
-            'mdl-badge--no-background': !!noBackground
-        }),
-        'data-badge': text
-    });
-};
+        return React.cloneElement(element, {
+            className: classNames(className, element.props.className, 'mdl-badge', {
+                'mdl-badge--overlap': !!overlap,
+                'mdl-badge--no-background': !!noBackground
+            }),
+            'data-badge': text
+        });
+    }
+}
 
 Badge.propTypes = propTypes;
 
