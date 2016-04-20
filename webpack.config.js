755c5688c317bf865a30ddc925d8756ecd1c90ec
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-04-20 18:00:47 +0200

    Fixed UMD output Expanded upon webpack externals config to differentiate between different output targets: * Import `react` and `react-dom` when using `require` * Import `React` and `ReactDOM` when using globals SEE: Dependency names wrong when using React MDL from script #298 SEE: ReactMDL.js bundle: "react" or "react-dom" instead of require('React') or require('ReactDOM') #267

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM'
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom'
+		}
     },
     devtool: 'sourcemap',
     module: {
