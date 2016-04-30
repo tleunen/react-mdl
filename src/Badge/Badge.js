0386d00ea5656d241b84e81af1ceb63f757ff6c4
Tommy Leunen <tommy.leunen+github@gmail.com>
2016-04-30 14:49:45 -0400

    perf(Badge): Change the component class for a function

--- src/Badge/Badge.js
+++ src/Badge/Badge.js
@@ -15,29 +15,28 @@ const propTypes = {
     noBackground: PropTypes.bool
 };
 
-class Badge extends React.Component {
-    render() {
-        const { children, className, text, overlap, noBackground } = this.props;
+const Badge = props => {
+    const { children, className, text, overlap, noBackground } = props;
 
-        // No badge if no children
-        if (!React.Children.count(children)) return null;
+    // No badge if no children
+    // TODO: In React 15, we can return null instead
+    if (!React.Children.count(children)) return <noscript />;
 
-        const element = typeof children === 'string'
-            ? <span>{children}</span>
-            : React.Children.only(children);
+    const element = typeof children === 'string'
+        ? <span>{children}</span>
+        : React.Children.only(children);
 
-        // No text -> No need of badge
-        if (text === null || typeof text === 'undefined') return element;
+    // No text -> No need of badge
+    if (text === null || typeof text === 'undefined') return element;
 
-        return React.cloneElement(element, {
-            className: classNames(className, element.props.className, 'mdl-badge', {
-                'mdl-badge--overlap': !!overlap,
-                'mdl-badge--no-background': !!noBackground
-            }),
-            'data-badge': text
-        });
-    }
-}
+    return React.cloneElement(element, {
+        className: classNames(className, element.props.className, 'mdl-badge', {
+            'mdl-badge--overlap': !!overlap,
+            'mdl-badge--no-background': !!noBackground
+        }),
+        'data-badge': text
+    });
+};
 
 Badge.propTypes = propTypes;
 
